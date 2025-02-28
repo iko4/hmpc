501ee1fee8121c9026d773793241e6a59d71fcde
iko4 <33726467+iko4@users.noreply.github.com>
2025-02-28 13:30:20 +0000

    fix(iter): use universal references to support more cases

--- hmpc/iter/collect_structure.hpp
+++ hmpc/iter/collect_structure.hpp
@@ -18,56 +18,56 @@ namespace hmpc::iter
     }
 
     template<hmpc::structure T, typename F>
-    constexpr auto collect(T& tuple, F const& f) HMPC_NOEXCEPT
+    constexpr auto collect(T&& tuple, F const& f) HMPC_NOEXCEPT
     {
         constexpr hmpc::size size = hmpc::typing::traits::structure_fields_v<T>;
         return hmpc::iter::for_packed_range<size>([&](auto... i)
         {
-            using first_type = detail::invoke_at_structure_field_t<F, 0, T&>;
-            constexpr bool all_same = (std::same_as<first_type, detail::invoke_at_structure_field_t<F, i, T&>> and ...);
-            using result_type = std::conditional_t<all_same, std::array<first_type, size>, std::tuple<detail::invoke_at_structure_field_t<F, i, T&>...>>;
+            using first_type = detail::invoke_at_structure_field_t<F, 0, T&&>;
+            constexpr bool all_same = (std::same_as<first_type, detail::invoke_at_structure_field_t<F, i, T>> and ...);
+            using result_type = std::conditional_t<all_same, std::array<first_type, size>, std::tuple<detail::invoke_at_structure_field_t<F, i, T>...>>;
             
             return result_type{f(at(tuple, i))...};
         });
     }
 
     template<typename F, typename... Ts>
-    constexpr auto collect(F const& f, Ts&... values) HMPC_NOEXCEPT
+    constexpr auto collect(F const& f, Ts&&... values) HMPC_NOEXCEPT
     {
         constexpr hmpc::size size = hmpc::typing::traits::structure_fields_v<Ts...>;
         return hmpc::iter::for_packed_range<size>([&](auto... i)
         {
-            using first_type = detail::invoke_at_structure_field_t<F, 0, Ts&...>;
-            constexpr bool all_same = (std::same_as<first_type, detail::invoke_at_structure_field_t<F, i, Ts&...>> and ...);
-            using result_type = std::conditional_t<all_same, std::array<first_type, size>, std::tuple<detail::invoke_at_structure_field_t<F, i, Ts&...>...>>;
+            using first_type = detail::invoke_at_structure_field_t<F, 0, Ts&&...>;
+            constexpr bool all_same = (std::same_as<first_type, detail::invoke_at_structure_field_t<F, i, Ts...>> and ...);
+            using result_type = std::conditional_t<all_same, std::array<first_type, size>, std::tuple<detail::invoke_at_structure_field_t<F, i, Ts...>...>>;
             
             return result_type{f(at(i, values...))...};
         });
     }
 
     template<hmpc::structure T, typename F>
-    constexpr auto collect_enumerated(T& tuple, F const& f) HMPC_NOEXCEPT
+    constexpr auto collect_enumerated(T&& tuple, F const& f) HMPC_NOEXCEPT
     {
         constexpr hmpc::size size = hmpc::typing::traits::structure_fields_v<T>;
         return hmpc::iter::for_packed_range<size>([&](auto... i)
         {
-            using first_type = detail::invoke_enumerate_structure_field_t<F, 0, T&>;
-            constexpr bool all_same = (std::same_as<first_type, detail::invoke_enumerate_structure_field_t<F, i, T&>> and ...);
-            using result_type = std::conditional_t<all_same, std::array<first_type, size>, std::tuple<detail::invoke_enumerate_structure_field_t<F, i, T&>...>>;
+            using first_type = detail::invoke_enumerate_structure_field_t<F, 0, T&&>;
+            constexpr bool all_same = (std::same_as<first_type, detail::invoke_enumerate_structure_field_t<F, i, T>> and ...);
+            using result_type = std::conditional_t<all_same, std::array<first_type, size>, std::tuple<detail::invoke_enumerate_structure_field_t<F, i, T>...>>;
 
             return result_type{f(i, at(tuple, i))...};
         });
     }
 
     template<typename F, typename... Ts>
-    constexpr auto collect_enumerated(F const& f, Ts&... values) HMPC_NOEXCEPT
+    constexpr auto collect_enumerated(F const& f, Ts&&... values) HMPC_NOEXCEPT
     {
         constexpr hmpc::size size = hmpc::typing::traits::structure_fields_v<Ts...>;
         return hmpc::iter::for_packed_range<size>([&](auto... i)
         {
-            using first_type = detail::invoke_enumerate_structure_field_t<F, 0, Ts&...>;
-            constexpr bool all_same = (std::same_as<first_type, detail::invoke_enumerate_structure_field_t<F, i, Ts&...>> and ...);
-            using result_type = std::conditional_t<all_same, std::array<first_type, size>, std::tuple<detail::invoke_enumerate_structure_field_t<F, i, Ts&...>...>>;
+            using first_type = detail::invoke_enumerate_structure_field_t<F, 0, Ts...>;
+            constexpr bool all_same = (std::same_as<first_type, detail::invoke_enumerate_structure_field_t<F, i, Ts...>> and ...);
+            using result_type = std::conditional_t<all_same, std::array<first_type, size>, std::tuple<detail::invoke_enumerate_structure_field_t<F, i, Ts...>...>>;
             
             return result_type{f(i, at(i, values...))...};
         });
